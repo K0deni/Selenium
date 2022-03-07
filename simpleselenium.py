14eab23fddd46757090f67225a37fd2f4ef20fa4
Denis <m0neymaker25@mail.ru>
2022-03-07 22:09:49 +0300

    Simple selenium test development

--- simpleselenium.py
+++
@@ -1,1 +0,0 @@
-##New selenium simple test project##
---
+++ test_homepage.py
@@ -0,0 +1,9 @@
+##New selenium simple test project##
+import pytest
+
+@pytest.mark.usefixtures('setup')
+
+class TestHomepage:
+    
+    def test_homepage(self):
+        pass
