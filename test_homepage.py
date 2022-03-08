4127cc8ffd52249184a4382c54e1319d60df7f39
Denis <m0neymaker25@mail.ru>
2022-03-08 22:50:43 +0300

    Selenium test modify

--- test_homepage.py
+++ test_homepage.py
@@ -1,11 +1,19 @@
 ##New selenium simple test project##
 import pytest
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 @pytest.mark.usefixtures('setup')
 
 class TestHomepage:
     
     def test_homepage(self):
-        pass
-    def test_google(self):
-        pass
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(10)
+        driver.find_element(By.CSS_SELECTOR, '#id_123')
+    
+
+        wait = WebDriverWait(driver, 15)
+        element = wait.until(ec.visibility_of_element_located(By.CSS_SELECTOR, '#id_123'))
+        
